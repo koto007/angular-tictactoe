c4af0394a1c944242a2696915afb0fcc6342f59e
koto007 <kotomiura@icloud.com>
2021-01-28 11:35:36 +0100

    Add a loop

--- src/app/game-page/game-page.component.ts
+++ src/app/game-page/game-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-page',
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamePageComponent implements OnInit {
 
-  constructor() { }
+  public grid: Array<any> = [];
+
+  private gridSize = 9;
+
+
+
+  constructor() {
+    for (let i = 0; i < this.gridSize; i++) {
+      this.grid.push(null);
+    }
+  }
 
   ngOnInit(): void {
   }
